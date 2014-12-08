2472a6a83a49aaf9d1f04ffe4aed1aa597bec408
david78k <david78k@gmail.com>
2014-12-07 19:31:46 -0500

    snn

---
+++ izhikevich.py
@@ -0,0 +1,8 @@
+from brian2 import *
+
+'''
+dvm/dt=(0.04/ms/mV)*vm**2+(5/ms)*vm+140*mV/ms-w : volt
+dw/dt=a*(b*vm-w)                            : volt/second
+'''
+
+#eqs = Izhikevich()
---
+++ snn.py
@@ -0,0 +1,62 @@
+from brian2 import *
+
+'''
+input: distance 
+- single value or coding to 100 values
+output: 
+- left motor power (signed)
+- right motor power (signed)
+'''
+
+N = 4
+tau = 20 * ms
+vt = -50*mV
+vr = -60*mV
+El = -49*mV # default -60
+psp = 0.5*mV
+
+G = NeuronGroup (N, model = 'dv/dt = - (v - El) / tau : volt', 
+                 threshold = 'v > vt', reset = 'v = vr')
+#G.v = 'vr + rand() * (vt - vr)'
+
+C = Synapses(G,G, post='v+=psp')
+C.connect('i==j', p=0.1)
+
+mon = StateMonitor(G, 'v', record = True)
+s_mon = SpikeMonitor(G) # for taster plot
+
+# poisson input stimuli
+P = PoissonGroup(N, np.arange(N)*Hz + 10*Hz)
+G2 = NeuronGroup (N, model = 'dv/dt = - (v - El) / tau : volt', 
+                  threshold = 'v > vt', reset = 'v = vr')
+S = Synapses(P,G2, pre='v+=1*mV', connect='i==j')
+#G2.v = 'vr + rand() * (vt - vr)'
+C2 = Synapses(G2,G2, post='v+=psp')
+C2.connect('i==j', p=0.1)
+
+mon2 = StateMonitor(G2, 'v', record = True)
+s_mon2 = SpikeMonitor(G2) # for taster plot
+
+run(200*ms)
+
+figure()
+subplot(211)
+# rastor plot
+plot(s_mon.t/ms, s_mon.i, '.')
+plot(s_mon2.t/ms, s_mon2.i, '.')
+
+subplot(212)
+plot(mon.t/ms, mon[0].v.T/mV)
+plot(mon2.t/ms, mon2[0].v.T/mV)
+
+xlabel('Time (in ms)')
+ylabel('Membrane potential (in mV)')
+#title('Membrane potential for neuron 0')
+legend(('normal','poisson'), 'upper left')
+
+show()
+
+#print mon.num_spikes
+print mon
+print mon.t
+print mon[0].v
