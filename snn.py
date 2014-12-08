4be6406abb8bde59efeffcb3d8fa5e9151e4ff5d
david78k <david78k@gmail.com>
2014-12-07 21:50:22 -0500

    snn function

--- snn.py
+++ snn.py
@@ -19,51 +19,57 @@ vr = -60*mV
 El = -49*mV # default -60
 psp = 0.5*mV
 
-I = NeuronGroup (inN, model = 'dv/dt = - (v - El) / tau : volt', 
-                 threshold = 'v > vt', reset = 'v = vr')
-O = NeuronGroup (outN, model = 'dv/dt = - (v - El) / tau : volt', 
-                 threshold = 'v > vt', reset = 'v = vr')
-O.v = 'vr + rand() * (vt - vr)'
+def snn(sensor_value): 
+    I = NeuronGroup (inN, model = 'dv/dt = - (v - El) / tau : volt', 
+                     threshold = 'v > vt', reset = 'v = vr')
+    O = NeuronGroup (outN, model = 'dv/dt = - (v - El) / tau : volt', 
+                     threshold = 'v > vt', reset = 'v = vr')
+    O.v = 'vr + rand() * (vt - vr)'
+    
+    C = Synapses(I,O, post='v+=psp', connect=True)
+    #C.connect('i==j', p=0.1)
+    
+    mon = StateMonitor(O, 'v', record = True)
+    s_mon = SpikeMonitor(O) # for taster plot
+    
+    '''
+    # poisson input stimuli
+    P = PoissonGroup(N, np.arange(N)*Hz + 10*Hz)
+    G2 = NeuronGroup (N, model = 'dv/dt = - (v - El) / tau : volt', 
+                      threshold = 'v > vt', reset = 'v = vr')
+    S = Synapses(P,G2, pre='v+=1*mV', connect='i==j')
+    #G2.v = 'vr + rand() * (vt - vr)'
+    C2 = Synapses(G2,G2, post='v+=psp')
+    C2.connect('i==j', p=0.1)
+    
+    mon2 = StateMonitor(G2, 'v', record = True)
+    s_mon2 = SpikeMonitor(G2) # for taster plot
+    '''
+    run(200*ms)
+    
+    figure()
+    subplot(211)
+    # rastor plot
+    plot(s_mon.t/ms, s_mon.i, '.')
+    #plot(s_mon2.t/ms, s_mon2.i, '.')
+    
+    subplot(212)
+    plot(mon.t/ms, mon[0].v.T/mV)
+    #plot(mon2.t/ms, mon2[0].v.T/mV)
+    
+    xlabel('Time (in ms)')
+    ylabel('Membrane potential (in mV)')
+    #title('Membrane potential for neuron 0')
+    #legend(('normal','poisson'), 'upper left')
+    
+    show()
+    
+    #print mon.t
+    #print mon[0].v.T
+    power = np.array(filter(None,mon[0].v.T),dtype='|S10').astype(np.longdouble)
+    return power
 
-C = Synapses(I,O, post='v+=psp', connect=True)
-#C.connect('i==j', p=0.1)
-
-mon = StateMonitor(O, 'v', record = True)
-s_mon = SpikeMonitor(O) # for taster plot
-
-'''
-# poisson input stimuli
-P = PoissonGroup(N, np.arange(N)*Hz + 10*Hz)
-G2 = NeuronGroup (N, model = 'dv/dt = - (v - El) / tau : volt', 
-                  threshold = 'v > vt', reset = 'v = vr')
-S = Synapses(P,G2, pre='v+=1*mV', connect='i==j')
-#G2.v = 'vr + rand() * (vt - vr)'
-C2 = Synapses(G2,G2, post='v+=psp')
-C2.connect('i==j', p=0.1)
-
-mon2 = StateMonitor(G2, 'v', record = True)
-s_mon2 = SpikeMonitor(G2) # for taster plot
-'''
-run(200*ms)
-
-figure()
-subplot(211)
-# rastor plot
-plot(s_mon.t/ms, s_mon.i, '.')
-#plot(s_mon2.t/ms, s_mon2.i, '.')
-
-subplot(212)
-plot(mon.t/ms, mon[0].v.T/mV)
-#plot(mon2.t/ms, mon2[0].v.T/mV)
-
-xlabel('Time (in ms)')
-ylabel('Membrane potential (in mV)')
-#title('Membrane potential for neuron 0')
-#legend(('normal','poisson'), 'upper left')
-
-show()
-
-#print mon.t
-#print mon[0].v.T
-print np.array(filter(None,mon[0].v.T),dtype='|S10').astype(np.longdouble)
-
+if __name__ == '__main__':
+    sensor_value = 35    
+    power = snn(sensor_value)
+    print power
