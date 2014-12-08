476ef2b221090cc3a7c20317acb0a24010998516
david78k <david78k@gmail.com>
2014-12-07 21:12:21 -0500

    in out of network

--- snn.py
+++ snn.py
@@ -1,30 +1,37 @@
 from brian2 import *
 
 '''
-input: distance 
-- single value or coding to 100 values
-output: 
-- left motor power (signed)
-- right motor power (signed)
+input: distance for ultrasonic or angular velocity for gyroscope (integer) 
+- single value or coding to multiple values
+- distance in cm: 1-250cm or 1-100inch + infinity
+- angular velocity: 0-440 degrees/sec
+output: motor power 0-100% (integer)
+- left motor power (signed): 101
+- right motor power (signed): 101
 '''
 
-N = 4
+#N = 251
+inN = 10
+outN = 8 # 202=101*2
 tau = 20 * ms
 vt = -50*mV
 vr = -60*mV
 El = -49*mV # default -60
 psp = 0.5*mV
 
-G = NeuronGroup (N, model = 'dv/dt = - (v - El) / tau : volt', 
+I = NeuronGroup (inN, model = 'dv/dt = - (v - El) / tau : volt', 
                  threshold = 'v > vt', reset = 'v = vr')
-#G.v = 'vr + rand() * (vt - vr)'
+O = NeuronGroup (outN, model = 'dv/dt = - (v - El) / tau : volt', 
+                 threshold = 'v > vt', reset = 'v = vr')
+O.v = 'vr + rand() * (vt - vr)'
 
-C = Synapses(G,G, post='v+=psp')
-C.connect('i==j', p=0.1)
+C = Synapses(I,O, post='v+=psp', connect=True)
+#C.connect('i==j', p=0.1)
 
-mon = StateMonitor(G, 'v', record = True)
-s_mon = SpikeMonitor(G) # for taster plot
+mon = StateMonitor(O, 'v', record = True)
+s_mon = SpikeMonitor(O) # for taster plot
 
+'''
 # poisson input stimuli
 P = PoissonGroup(N, np.arange(N)*Hz + 10*Hz)
 G2 = NeuronGroup (N, model = 'dv/dt = - (v - El) / tau : volt', 
@@ -36,23 +43,23 @@ C2.connect('i==j', p=0.1)
 
 mon2 = StateMonitor(G2, 'v', record = True)
 s_mon2 = SpikeMonitor(G2) # for taster plot
-
+'''
 run(200*ms)
 
 figure()
 subplot(211)
 # rastor plot
 plot(s_mon.t/ms, s_mon.i, '.')
-plot(s_mon2.t/ms, s_mon2.i, '.')
+#plot(s_mon2.t/ms, s_mon2.i, '.')
 
 subplot(212)
 plot(mon.t/ms, mon[0].v.T/mV)
-plot(mon2.t/ms, mon2[0].v.T/mV)
+#plot(mon2.t/ms, mon2[0].v.T/mV)
 
 xlabel('Time (in ms)')
 ylabel('Membrane potential (in mV)')
 #title('Membrane potential for neuron 0')
-legend(('normal','poisson'), 'upper left')
+#legend(('normal','poisson'), 'upper left')
 
 show()
 
